264f80f8aeae901f47000c9ba0d056baf7aeaa79
Leann Kim <leannminakim@gmail.com>
2018-03-25 19:15:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?.scan(/[aeiou]+/)
-
+  starts_with_a_vowel?
 end
 
 def words_starting_with_un_and_ending_with_ing.scan(/(un)+(ing)/)
-
+  words_starting_with_un_and_ending_with_ing
 end
 
 def words_five_letters_long.scan(/\w{5}/)
-
+  words_five_letters_long
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
