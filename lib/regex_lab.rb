e6a8bf290e96e2b4261a4c6128e5e45780de4724
Leann Kim <leannminakim@gmail.com>
2018-03-25 19:15:12 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,12 @@
-def starts_with_a_vowel?.scan (/[aeiou]+/)
+def starts_with_a_vowel?.scan(/[aeiou]+/)
 
 end
 
-def words_starting_with_un_and_ending_with_ing.scan (/(un)+(ing)/)
+def words_starting_with_un_and_ending_with_ing.scan(/(un)+(ing)/)
 
 end
 
-def words_five_letters_long.scan (/\w{5}/)
+def words_five_letters_long.scan(/\w{5}/)
 
 end
 
